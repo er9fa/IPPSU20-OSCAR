4afc367f1eace38a4aa6156325643b5026390d1b
Ned Rossman <er9fa@virginia.edu>
2020-11-18 12:10:05 -0500

    Added proxy for satellite API fetch

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require('express')
 const path = require('path')
+const fetch = require('node-fetch')
+
+const app = express();
+
+
+app.get('/api/v1/corsbypass', (req, res) => {
+  sat_key = req.query.sat_key;
+  user_lat = req.query.user_lat;
+  user_long = req.query.user_long;
+
+  fetch(`https://api.n2yo.com/rest/v1/satellite/visualpasses/${sat_key}/${user_lat}/${user_long}/0/1/120/&apiKey=WXKLJR-AMJM8S-Y9GBGH-4L7F`)
+  .catch(err => console.log(err))
+  .then(data => {
+    res.header('Access-Control-Allow-Origin', '*');
+  });
+});
+
+
+
+
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/nedspage', (req, res) => res.render('pages/nedspage'))
-  .get('/katiespage', (req, res) => res.render('pages/katiespage'))
-  .get('/renitaspage', (req, res) => res.render('pages/renitaspage'))
-  .get('/davidspage', (req, res) => res.render('pages/davidspage'))
-  .get('/iss', (req, res) => res.render('pages/iss'))
-  .get('/game', (req, res) => res.render('pages/game'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/nedspage', (req, res) => res.render('pages/nedspage'))
+app.get('/katiespage', (req, res) => res.render('pages/katiespage'))
+app.get('/renitaspage', (req, res) => res.render('pages/renitaspage'))
+app.get('/davidspage', (req, res) => res.render('pages/davidspage'))
+app.get('/iss', (req, res) => res.render('pages/iss'))
+app.get('/game', (req, res) => res.render('pages/game'))
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
